e3b2a6ebb75c5babf07824b462e926c9149c2c41
AngJianming <angjianming2005@hotmail.com>
2024-12-06 09:14:20 +0800

    updates

--- source/admin/newsletterdel.php
+++ source/admin/newsletterdel.php
@@ -2,12 +2,11 @@
 
 include ('db.php');
 
-$id=$_GET['eid'];
-if($id=="")
+$id = $_GET['eid'];
+if ($id == "")
 {
-echo '<script>alert("Sorry ! Wrong Entry") </script>' ;
-		header("Location: messages.php");
-
+	echo '<script>alert("Sorry ! Wrong Entry") </script>' ;
+	header("Location: messages.php");
 
 }
 
